506de3b5405b8c7d5e3ca4799a9e85441fbf00a2
ds.vasudev <vasudevds@gmail.com>
2024-05-03 10:24:51 +0530

    Updated

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -24,8 +24,42 @@ const createOrder=async(req,res,next)=>{
 
 }
 
+const validatePayment = async (req, res, next) => {
+    try {
+      const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+        req.body;
+      const user = { id: 1 };
+      let data = await instance.payments.fetch(razorpay_payment_id);
+      const { amount, id, currency, status, order_id, method, description } =
+        data;
 
+      if (status === "captured") {
+        return res.status(200).json({success:true,message:"Your Transaction is Successfull",data:{
+            data
+        }})
+      }
+  
+      let trans = await Transaction.create({
+        userId: user.id,
+        orderId: order_id,
+        payment_type: method,
+        status,
+        currency,
+        paymentId: id,
+        amount: amount / 100,
+      });
+  
+      res
+        .status(200)
+        .json({ success: true, message: "payment-successfull", payment: trans });
+    } catch (error) {
+      console.log(error.message);
+      next(error);
+    }
+  };
+  
 
 module.exports={
-    createOrder
+    createOrder,
+    validatePayment
 }
--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -4,5 +4,7 @@ const paymentController=require("./../controllers/paymentController")
 
 router.post("/create-payment-order",paymentController.createOrder)
 
+router.post("/validate-payment",paymentController.validatePayment)
+
 
 module.exports=router;
